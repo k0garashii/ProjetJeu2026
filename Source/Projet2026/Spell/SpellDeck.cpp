3c6f3d33ba3cfd2dbf342fd0b70c0905f19336db
Menez sasha <s.menez@student.isartdigital.com>
2025-12-22 16:34:43 +0100

    add: cooldown on spell Launch

--- Source/Projet2026/Spell/SpellDeck.cpp
+++ Source/Projet2026/Spell/SpellDeck.cpp
@@ -4,6 +4,14 @@ USpellDeck::USpellDeck()
 {
 	SpellData.Init(nullptr, 5);
 	ActiveSpell = nullptr;
+	
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void USpellDeck::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	UpdateCooldowns(DeltaTime);
 }
 
 void USpellDeck::SetSpellData(USpellData* spellData, int index)
@@ -15,15 +23,42 @@ void USpellDeck::SetSpellData(USpellData* spellData, int index)
 }
 
 void USpellDeck::SetActiveSpell(int index)
-{
-	if (index >= 0 && index < 5)
-		ActiveSpell = SpellData[index];
+{	
+	ActiveSpell = SpellData[index];
 }
 
 void USpellDeck::LaunchSpell(AActor* actor)
 {
+	if (IsOnCooldown(ActiveSpell))
+		return;
+	
 	if (ActiveSpell && ActiveSpell->SpellForm)
 	{
 		ActiveSpell->SpellForm->InitializeSpellForm(actor, ActiveSpell->Prefab);
+		Spells.Add(CooldownSpell(ActiveSpell, ActiveSpell->Cooldown));
 	}
+}
+
+void USpellDeck::UpdateCooldowns(float DeltaTime)
+{
+	for (int i = Spells.Num() - 1; i >= 0; --i)
+	{
+		Spells[i].RemainingCooldown -= DeltaTime;
+		if (Spells[i].RemainingCooldown <= 0)
+		{
+			Spells.RemoveAt(i);
+		}
+	}
+}
+
+bool USpellDeck::IsOnCooldown(USpellData* spellData)
+{
+	for (const CooldownSpell& cooldownSpell : Spells)
+	{
+		if (spellData == cooldownSpell.SpellData)
+		{
+			return true;
+		}
+	}
+	return false;
 }
--- Source/Projet2026/Spell/SpellDeck.h
+++ Source/Projet2026/Spell/SpellDeck.h
@@ -4,6 +4,16 @@
 #include "Spell/SpellData.h"
 #include "SpellDeck.generated.h"
 
+struct CooldownSpell
+{
+	CooldownSpell(USpellData* InSpellData, float InRemainingCooldown)
+		: SpellData(InSpellData), RemainingCooldown(InRemainingCooldown)
+	{}
+	
+	USpellData* SpellData;
+	float RemainingCooldown;
+};
+
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class PROJET2026_API USpellDeck : public UActorComponent
 {
@@ -11,11 +21,15 @@ class PROJET2026_API USpellDeck : public UActorComponent
 public:
 	USpellDeck();
 	
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 	void SetSpellData(USpellData* spellData, int index);
 	void SetActiveSpell(int index);
 	void LaunchSpell(AActor* actor);
+	void UpdateCooldowns(float DeltaTime);
+	bool IsOnCooldown(USpellData* spellData);
 	
 	UPROPERTY(EditAnywhere, Category = "Spell")
 	TArray<USpellData*> SpellData;
+	TArray<CooldownSpell> Spells;
 	USpellData* ActiveSpell = nullptr;
 };
